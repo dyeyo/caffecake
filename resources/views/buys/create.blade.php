eab06de49be14fc2797c08f3c09be41271781cfd
Diego Vallejo <dyegovallejob@gmail.com>
2020-06-11 16:06:49 -0500

    typo bienvenidos 2

--- resources/views/buys/create.blade.php
+++ resources/views/buys/create.blade.php
@@ -3,7 +3,7 @@
 <div class="container-fluid">
   <div class="row page-titles">
     <div class="col-md-5 align-self-center">
-      <h4 class="text-themecolor">Bienvendido a WaffCake</h4>
+      <h4 class="text-themecolor">Bienvenido a WaffCake</h4>
     </div>
     <div class="col-md-7 align-self-center text-right">
       <div class="d-flex justify-content-end align-items-center">
